9ddc3f2201268c50f92afceb0e38c9ca42fe3d65
Leonardho Rante S <flazen.edu@gmail.com>
2025-01-24 03:10:08 +0700

    [FE] Show Total All Monthly Used Outfit

--- wardrobe/src/app/clothes/generated/page.js
+++ wardrobe/src/app/clothes/generated/page.js
@@ -4,6 +4,7 @@ import AtomsBreakLine from "../../../components/atoms/atoms_breakline";
 import MoleculesFooter from "../../../components/molecules/molecules_footer";
 import GeneratedSectionRandomOutift from "./sections/generated_random_outfit";
 import GeneratedSectionShowAllOutfit from "./sections/generated_show_all_outfit";
+import GeneratedSectionOutfitMonthlyTotalUsed from "./sections/generated_outfit_monthly_total_used";
 
 export default function GeneratedPage() {
     return (
@@ -11,6 +12,8 @@ export default function GeneratedPage() {
             <OrganismsNavbar/>
             <AtomsBreakLine length={3}/>
             <GeneratedSectionRandomOutift/>
+            <AtomsBreakLine length={3}/>
+            <GeneratedSectionOutfitMonthlyTotalUsed/>
             <AtomsBreakLine length={5}/>
             <GeneratedSectionShowAllOutfit ctx={"all_outfit"}/>
             <MoleculesFooter/>
